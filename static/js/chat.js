0be3251e11f68fd039f1f3068ffae9cc5a1b6a9b
younessamsong09-hue <younessamsong09@gmail.com>
2026-03-24 21:26:29 +0100

    تحديث البوابة السيادية V4: لوحة تحكم سرية، خلفية احترافية، ونظام إحصائيات 🇲🇦💎

--- static/js/chat.js
+++ static/js/chat.js
@@ -44,3 +44,40 @@ function smartSearch() {
         }
     });
 }
+// خلفية متحركة خفيفة
+const canvas = document.getElementById('bg-canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particles = [];
+class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 2;
+        this.speedX = Math.random() * 0.5 - 0.25;
+        this.speedY = Math.random() * 0.5 - 0.25;
+    }
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.01;
+    }
+    draw() {
+        ctx.fillStyle = '#daa52033';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (particles.length < 50) particles.push(new Particle());
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].draw();
+        if (particles[i].size <= 0.2) { particles.splice(i, 1); i--; }
+    }
+    requestAnimationFrame(animate);
+}
+animate();
